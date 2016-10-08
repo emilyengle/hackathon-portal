2156d73b3891b71d6e30cbdd32a8b6bcb5ce5ca1
Kevin <unkrich.5@osu.edu>
2016-10-08 08:51:31 -0400

    Sets sponsor invite code beforehand

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,8 +1,15 @@
+require 'securerandom'
 class Sponsor < ApplicationRecord
-  has_many :pledges, :dependent => :destroy
-  has_many :sponsor_tasks
-  has_many :tasks, :through => :sponsor_tasks
+  before_validation :set_registration_password, :on => :create
 
+
+  has_many :pledges, :dependent => :destroy
   validates :name, :presence => true
   validates :registration_password, :presence => true
+
+
+  private 
+	  def set_registration_password
+		self.registration_password = SecureRandom.hex(20)
+	  end
 end
