19c365f2eb1c00f14f76622a2c05670598aac9ea
Kevin <unkrich.5@osu.edu>
2016-10-08 04:13:11 -0400

    Added a few more relationships to sponsor model

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,5 +1,7 @@
 class Sponsor < ApplicationRecord
   has_many :pledges, :dependent => :destroy
+  has_many :sponsor_tasks
+  has_many :tasks, :through => :sponsor_tasks
 
   validates :name, :presence => true
   validates :registration_password, :presence => true
