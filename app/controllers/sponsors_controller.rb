54cc19a8e1f3a98c0ec1b2b96e95349ac1c07b43
Kevin <unkrich.5@osu.edu>
2016-10-08 11:01:56 -0400

    Probable first attempt at sponsors controller

---
+++ app/controllers/sponsors_controller.rb
@@ -0,0 +1,47 @@
+class SponsorsController < ApplicationController
+  
+  def index
+    @sponsors = Sponsor.all
+  end
+
+  def show
+    @sponsor = Sponsor.find(params[:id])
+  end
+
+  def new
+    @sponsor = Sponsor.new
+  end
+
+  def create
+    @sponsor = Sponsor.new(params[:sponsor])
+
+    respond_to do |format|
+      if @sponsor.save
+        format.html { redirect_to @sponsor, notice: 'Sponsor was successfully created.' }
+        format.json { render json: @sponsor, status: :created, location: @sponsor }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @sponsor.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  def edit
+    @sponsor = Sponsor.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @sponsor = Sponsor.find(params[:id])
+    @sponsor.destroy
+
+    respond_to do |format|
+      format.html { redirect_to sponsors_url }
+      format.json { head :no_content }
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'sponsors/index'
+
+  get 'sponsors/show'
+
+  get 'sponsors/new'
+
+  get 'sponsors/create'
+
+  get 'sponsors/edit'
+
+  get 'sponsors/update'
+
+  get 'sponsors/destroy'
+
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
   get "/logout" => "sessions#destroy"
