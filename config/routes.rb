f069c40070daa9d188d744fc6d2aec55dadffae6
Emily Engle <engle.165@osu.edu>
2016-10-08 11:55:57 -0400

    Updated tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-
-  get 'tasks/show'
-
-  get 'tasks/new'
-
-  get 'tasks/create'
-
-  get 'tasks/edit'
-
-  get 'tasks/update'
-
-  get 'tasks/destroy'
-
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
   get "/logout" => "sessions#destroy"
